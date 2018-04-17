9a7d2ef2f495257ba38f9b34f5c047992a068cf3
Mariano <batiar@gmail.com>
2018-04-17 19:25:11 -0300

    Cambios diseño

--- src/main/app/src/components/Nav.js
+++ src/main/app/src/components/Nav.js
@@ -1,58 +1,63 @@
 import React, { Component } from 'react';
-import  {Link} from 'react-router-dom';
-import {connect} from 'react-redux'
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux'
 
 class Nav extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
-    
-    this.state={
-        navCollapsed:true
+
+        this.state = {
+            navCollapsed: false
+        }
     }
-}
     _onToggleNav = () => {
-        console.log("Click "+JSON.stringify(this.props.userData));
-        this.setState({navCollapsed : !this.state.navCollapsed})
+        console.log("Click " + JSON.stringify(this.props.userData));
+        this.setState({ navCollapsed: !this.state.navCollapsed })
     }
-    
+
     render() {
-        const {navCollapsed} =this.state
-        let collapseClass='collapse'
-        if(!navCollapsed)
-            collapseClass+=' navbar-collapse';
-        let status=<li className="nav-item">
-        <Link to='/user/logout' className="nav-link" >Logout</Link>
+        const { navCollapsed } = this.state
+        console.log("COLLA " + navCollapsed);
+        let collapseClass = 'collapse'
+        if (!navCollapsed)
+            collapseClass += ' navbar-collapse';
+        let status = <li className="nav-item">
+            <Link to='/user/logout' className="nav-link" >Logout</Link>
         </li>
-        if(!this.props.loggedIn){
-            status=<li className="nav-item">
-            <Link to='/user/login' className="nav-link" >Login</Link><Link to='/user/signup' className="nav-link" >Signup</Link>
+        if (!this.props.loggedIn) {
+            status = <li className="nav-item">
+                <Link to='/user/login' className="nav-link" >Login</Link><Link to='/user/signup' className="nav-link" >Signup</Link>
             </li>
         }
-        return (<nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <Link to='/' className="navbar-brand">Weather App Challenge</Link>
-            
-
-            <button onClick={this._onToggleNav} className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor03" aria-controls="navbarColor03"
-                aria-expanded="false" aria-label="Toggle navigation">
-                <span className="navbar-toggler-icon"></span>
-            </button>
-
-            <div className={collapseClass} id="navbarColor03">
-                <ul className="navbar-nav ml-auto">
-                    <li className="nav-item active">
-                    <Link to='/' className="nav-link" >Boards
-            <span className="sr-only">(current)</span>
-                        </Link>
-                    </li>
-                    {status}
-                </ul>
-
-            </div>
-
-        </nav>)
+        return (
+            <nav class="navbar navbar-default">
+                <div class="container-fluid">
+                    <div class="navbar-header">
+                        <Link to='/' className="navbar-brand" >Weather App Challenge</Link>
+                    </div>
+                    <ul class="nav navbar-nav navbar-right">
+                        <li>
+                            <Link to='/user/signup' className="nav-link" >
+                                <i class="fas fa-user-plus" ></i> Sing Up
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/user/login' className="nav-link" >
+                                <i class="fas fa-sign-in-alt"></i> Login
+                            </Link>
+                        </li>
+                        <li>
+                            <Link to='/user/logout' className="nav-link" >
+                                <i class="fas fa-sign-out-alt"></i> Logout
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        )
     }
 }
-const mapStateToProps = state =>({
+const mapStateToProps = state => ({
     userData: state.user.userData,
     loggedIn: state.user.loggedIn
 })
